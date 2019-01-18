600d31a1af01c17e31c09527245ace8e0645e212
majb <920356823@qq.com>
2019-01-18 23:08:18 +0800

    modify

--- get_title_from_pdf.py
+++ get_title_from_pdf.py
@@ -4,7 +4,7 @@
 @file: get_title_from_pdf.py
 """
 from PyPDF2 import PdfFileReader
-import os, datetime
+import os
 
 
 def format_str(old_str):
@@ -32,23 +32,28 @@ def get_format_num(position):
 
 def get_title(path):
     for root, dirs, files in os.walk(path):
-        print(files)
         files = sorted(files, key=lambda x: os.path.getmtime(os.path.join(path, x)), reverse=False)
         title = str()
+
         for position in range(0, len(files)):
             file_path = path + "\\" + files[position]
-            time_stamp = os.path.getmtime(file_path)
-            date = datetime.datetime.fromtimestamp(time_stamp)
-            print("修改时间: {}".format(date.strftime("%Y-%m-%d %H:%M:%S")), end=" ")
-            with open(file_path, "rb") as f:
-                pdf_reader = PdfFileReader(f)
-                title = pdf_reader.getDocumentInfo().title
-                print(title)
-
-            title = format_str(title)
-            sorted_num = get_format_num(position)
-            os.rename(file_path, path + "\\" + sorted_num + "_" + title + ".pdf")
+            try:
+                with open(file_path, "rb") as f:
+                    pdf_reader = PdfFileReader(f)
+                    title = pdf_reader.getDocumentInfo().title
+                    print(title)
+                    f.close()
+                if title is None or len(title) == 0:
+                    title = files[position]
+                else:
+                    title = format_str(title)
+                sorted_num = get_format_num(position)
+                os.rename(file_path, path + "\\" + sorted_num + "_" + title + ".pdf")
+            except Exception as e:
+                print(e)
+                sorted_num = get_format_num(position)
+                os.rename(file_path, path + "\\" + sorted_num + "_.pdf")
 
 
 if __name__ == '__main__':
-    get_title("D:\\PycharmProjects\\batch_rename_file\\file")
+    get_title("C:\\Users\\sky\\Desktop\\2016")
